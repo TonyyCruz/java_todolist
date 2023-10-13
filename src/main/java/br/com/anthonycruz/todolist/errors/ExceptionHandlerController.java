fc23eba7ebd4ae9e7137e270fd552261ed5f25bd
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-10-13 18:19:57 -0300

    chore: adiciona tratamento de excecao para fotmato de data incorreto

--- src/main/java/br/com/anthonycruz/todolist/errors/ExceptionHandlerController.java
+++ src/main/java/br/com/anthonycruz/todolist/errors/ExceptionHandlerController.java
@@ -1,5 +1,7 @@
 package br.com.anthonycruz.todolist.errors;
 
+import java.time.format.DateTimeParseException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,5 +21,11 @@ public class ExceptionHandlerController {
   @ExceptionHandler(UserException.class)
   public ResponseEntity<String> handleUserException(UserException e) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+  }
+
+  @ExceptionHandler(DateTimeParseException.class)
+  public ResponseEntity<String> handleDateException(DateTimeParseException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        "Invalid date format, please insert in the format (yyyy-mm-ddT00:00:00).");
   }
 }
